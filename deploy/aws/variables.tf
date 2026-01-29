5af5f948d68dbc9b79bba7b9cd3f01929ade6dc9
mattkeeble <mkee@hotmail.co.uk>
2026-01-29 11:11:00 +0000

    initial commit

---
+++ deploy/aws/variables.tf
@@ -0,0 +1,24 @@
+variable "app_environment" {
+    type = string
+} 
+
+variable "app_version" {
+    type = string
+}
+
+variable "app_repo" {
+    type = string
+}
+
+variable "infra_environment" {
+    type = string
+}
+
+variable "base_alb_listener_rule_priority" {
+    type = number
+}
+
+
+variable "az_use_count" {
+    type = number
+}
---
+++ servicetest/karate/actuator.feature
@@ -0,0 +1,8 @@
+Feature: Call the health check endpoint
+
+Scenario: Successful call to health check endpoint
+    Given url 'http://localhost:9080'
+    And path 'default/chameleon/actuator/health'
+    When method GET
+    Then status 200
+    * match response.status == 'UP'
